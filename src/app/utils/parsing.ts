5864ce71e4384b155fe999d3354df69f79b8c80c
jjaison <joeljaison13@berkeley.edu>
2023-07-22 15:18:18 -0700

    added parsing script

---
+++ src/app/utils/parsing.ts
@@ -0,0 +1,109 @@
+import cheerio from 'cheerio';
+import { NodeHtmlMarkdown } from 'node-html-markdown';
+
+interface Page {
+  url: string;
+  content: string;
+}
+
+class Crawler {
+  private seen = new Set<string>();
+  private pages: Page[] = [];
+  private queue: { url: string; depth: number }[] = [];
+
+  constructor(private maxDepth = 2, private maxPages = 1) { }
+
+  async crawl(startUrl: string): Promise<Page[]> {
+    // Add the start URL to the queue
+    this.addToQueue(startUrl);
+
+    // While there are URLs in the queue and we haven't reached the maximum number of pages...
+    while (this.shouldContinueCrawling()) {
+      // Dequeue the next URL and depth
+      const { url, depth } = this.queue.shift()!;
+
+      // If the depth is too great or we've already seen this URL, skip it
+      if (this.isTooDeep(depth) || this.isAlreadySeen(url)) continue;
+
+      // Add the URL to the set of seen URLs
+      this.seen.add(url);
+
+      // Fetch the page HTML
+      const html = await this.fetchPage(url);
+
+      // Parse the HTML and add the page to the list of crawled pages
+      this.pages.push({ url, content: this.parseHtml(html) });
+
+      // Extract new URLs from the page HTML and add them to the queue
+      this.addNewUrlsToQueue(this.extractUrls(html, url), depth);
+    }
+
+    // Return the list of crawled pages
+    return this.pages;
+  }
+
+  private isTooDeep(depth: number) {
+    return depth > this.maxDepth;
+  }
+
+  private isAlreadySeen(url: string) {
+    return this.seen.has(url);
+  }
+
+  private shouldContinueCrawling() {
+    return this.queue.length > 0 && this.pages.length < this.maxPages;
+  }
+
+  private addToQueue(url: string, depth = 0) {
+    this.queue.push({ url, depth });
+  }
+
+  private addNewUrlsToQueue(urls: string[], depth: number) {
+    this.queue.push(...urls.map(url => ({ url, depth: depth + 1 })));
+  }
+
+  private async fetchPage(url: string): Promise<string> {
+    try {
+      const response = await fetch(url);
+      return await response.text();
+    } catch (error) {
+      console.error(`Failed to fetch ${url}: ${error}`);
+      return '';
+    }
+  }
+
+  private parseHtml(html: string): string {
+    const $ = cheerio.load(html);
+    $('a').removeAttr('href');
+    return NodeHtmlMarkdown.translate($.html());
+  }
+
+  private extractUrls(html: string, baseUrl: string): string[] {
+    const $ = cheerio.load(html);
+    const relativeUrls = $('a').map((_, link) => $(link).attr('href')).get() as string[];
+    return relativeUrls.map(relativeUrl => new URL(relativeUrl, baseUrl).href);
+  }
+}
+
+async function get_content(url:string) {
+  const crawler = new Crawler()
+  const page = await crawler.crawl(url)
+  const timestampAsString = generateTimestampAsString();
+  const page_content = timestampAsString + page![0].content
+  console.log(page_content)
+}
+
+
+function generateTimestampAsString(): string {
+  const now = new Date();
+  const year = now.getFullYear().toString().padStart(4, '0');
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+get_content("https://www.gong.io/")
