29a90668c90d9c00b396f45ee6dee86ee3796f95
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-24 03:31:45 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+          net: false,
+          path: false,
+          http: false,
+          https: false,
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
