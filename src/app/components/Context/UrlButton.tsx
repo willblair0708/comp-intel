e6273a7b64d2fae5617bbec60b9b69f29500711f
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-24 02:28:39 -0400

    update button

--- src/app/components/Context/UrlButton.tsx
+++ src/app/components/Context/UrlButton.tsx
@@ -32,6 +32,7 @@ const UrlButton: FC<IURLButtonProps> = ({ entry, onClick }) => (
     >
       <a
         href={entry.url}
+        download
         target="_blank"
         rel="noopener noreferrer"
         className="bg-blue-500 hover:bg-blue-600 active:bg-brand-700 dark:bg-brand-400 dark:hover:bg-brand-300 dark:active:bg-brand-200 flex items-center justify-center rounded-full p-2 text-3xl text-white transition duration-200 hover:cursor-pointer dark:text-white"
