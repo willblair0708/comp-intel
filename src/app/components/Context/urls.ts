7ec64e7cc5d31c9abcf299372dd4cc7b42bc326a
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-24 04:49:27 -0400

    update url

--- src/app/components/Context/urls.ts
+++ src/app/components/Context/urls.ts
@@ -1,5 +1,5 @@
 export const urls = [{
-  url: "https://docs.google.com/spreadsheets/d/1JG1ae3I9dLsy1Gx9SzQs8zXXuGMAbIm-TlpeIwUZ0DQ/export?format=csv",
+  url: "https://raw.githubusercontent.com/willblair0708/comp-intel/main/public/linkedin.csv",
   title: "KPFellows",
   seeded: false,
   loading: false,
