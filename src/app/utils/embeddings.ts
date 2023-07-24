1715e671afa9635dbeed2300f7dbd4510f7571d3
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-24 13:45:39 -0400

    update

--- src/app/utils/embeddings.ts
+++ src/app/utils/embeddings.ts
@@ -1,24 +1,20 @@
+
+import { OpenAIApi, Configuration } from "openai-edge";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+
 export async function getEmbeddings(input: string) {
   try {
-    const response = await limiter.schedule(() => openai.createEmbedding({
+    const response = await openai.createEmbedding({
       model: "text-embedding-ada-002",
       input: input.replace(/\n/g, ' ')
-    }));
-
-    // Check if the response is valid
-    if (!response.ok) {
-      console.error(`OpenAI API responded with status ${response.status}: ${response.statusText}`);
-      throw new Error(`OpenAI API responded with status ${response.status}: ${response.statusText}`);
-    }
+    })
 
     const result = await response.json();
-    if (result.data && result.data.length > 0) {
-      return result.data[0].embedding as number[]
-    } else {
-      // Handle the case when data is not available
-      console.error('No data available in the response');
-      return []; // You should decide what to return or throw in this case
-    }
+    return result.data[0].embedding as number[]
 
   } catch (e) {
     console.log("Error calling OpenAI embedding API: ", e);
