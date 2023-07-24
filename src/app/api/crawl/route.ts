bef5c453f32a053017ac8efe87ac9751c4b90e1d
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-23 23:37:52 -0400

    update route.ts

--- src/app/api/crawl/route.ts
+++ src/app/api/crawl/route.ts
@@ -1,14 +1,12 @@
-// route.ts
-
 import seed from './seed'
 import { NextResponse } from 'next/server';
 
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
-  const { url, limit, indexName, options } = await req.json()
+  const { url, options } = await req.json()
   try {
-    const documents = await seed(url, limit, indexName, options)
+    const documents = await seed(url, 1, process.env.PINECONE_INDEX!, options)
     return NextResponse.json({ success: true, documents })
   } catch (error) {
     return NextResponse.json({ success: false, error: "Failed crawling" })
