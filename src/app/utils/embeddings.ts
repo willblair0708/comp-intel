c71c7ded16d41f2b25f2734c1bea96a4b254dcc7
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-07-24 14:13:58 -0400

    Update embeddings.ts

--- src/app/utils/embeddings.ts
+++ src/app/utils/embeddings.ts
@@ -1,4 +1,3 @@
-
 import { OpenAIApi, Configuration } from "openai-edge";
 
 const config = new Configuration({
@@ -13,13 +12,8 @@ export async function getEmbeddings(input: string) {
       input: input.replace(/\n/g, ' ')
     })
 
-    // Wait for 1 second before returning the result
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        const result = await response.json();
-        resolve(result.data[0].embedding as number[]);
-      }, 1000);
-    });
+    const result = await response.json();
+    return result.data[0].embedding as number[]
 
   } catch (e) {
     console.log("Error calling OpenAI embedding API: ", e);
