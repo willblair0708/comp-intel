64fc93d0e0a2625d6ec738612f0e6a68dba3bdba
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-07-24 00:10:01 -0400

    update crawl

--- src/app/api/crawl/crawler.ts
+++ src/app/api/crawl/crawler.ts
@@ -1,89 +1,35 @@
-import csvParser from 'csv-parser';
 import axios from 'axios';
-import { promisify } from 'util';
-import stream, { pipeline } from 'stream';
+import Papa from 'papaparse';
 
-class Person {
-  constructor(
-    public Name: string,
-    public Class: string,
-    public URL: string,
-    public Email: string,
-    public Company: string,
-    public Position: string,
-    public Location: string,
-    public Skills: string,
-    public PastJobTitles: string,
-    public Organizations: string,
-    public About: string,
-  ) { }
+interface Page {
+  url: string;
+  content: any[];
 }
 
-class CsvCrawler {
-  private people: Person[] = [];
-
-  constructor(private maxPeople = 250) { }
-
-  async crawl(csvUrl: string): Promise<Person[]> {
-    // Read the CSV file from URL and add people to the queue
-    await this.addToQueueFromCsv(csvUrl);
-
-    // Return the list of crawled people
-    return this.people;
+class Crawler {
+  async crawl(url: string): Promise<Page> {
+    const csv = await this.fetchPage(url);
+    return {
+      url,
+      content: this.parseCsv(csv),
+    };
   }
 
-  private async addToQueueFromCsv(csvUrl: string) {
-    const response = await axios.get(csvUrl, { responseType: 'stream' });
-    const asyncPipeline = promisify(pipeline);
-
-    return new Promise<void>((resolve, reject) => {
-      asyncPipeline(
-        response.data,
-        csvParser(),
-        new stream.Writable({
-          objectMode: true,
-          write: (data: any, _: string, callback: (error?: Error | null) => void) => {
-            const person = new Person(
-              data.Name,
-              data.Class,
-              data.URL,
-              data.Email,
-              data.Company,
-              data.Position,
-              data.Location,
-              data.Skills,
-              data.PastJobTitles,
-              data.Organizations,
-              data.About,
-            );
-            this.addToQueue(person);
-            callback();
-          },
-        })
-      )
-      .then(resolve)
-      .catch(reject);
-    });
-  }
-
-  private addToQueue(person: Person) {
-    if (this.people.length < this.maxPeople) {
-      this.people.push(person);
+  private async fetchPage(url: string): Promise<string> {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error(`Failed to fetch ${url}: ${error}`);
+      return '';
     }
   }
+
+  private parseCsv(csv: string): any[] {
+    const parsed = Papa.parse(csv, { header: true });
+    return parsed.data;
+  }
 }
 
-export function generateTimestampAsString(): string {
-  const now = new Date();
-  const year = now.getFullYear().toString().padStart(4, '0');
-  const month = (now.getMonth() + 1).toString().padStart(2, '0');
-  const day = now.getDate().toString().padStart(2, '0');
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
-
-export { CsvCrawler };
-export type { Person };
+export { Crawler };
+export type { Page };
--- src/app/api/crawl/seed.ts
+++ src/app/api/crawl/seed.ts
@@ -3,33 +3,33 @@ import { Document, MarkdownTextSplitter, RecursiveCharacterTextSplitter } from "
 import { utils as PineconeUtils, Vector } from "@pinecone-database/pinecone";
 import md5 from "md5";
 import { getPineconeClient } from "@/utils/pinecone";
-import { CsvCrawler, Person, generateTimestampAsString } from "./crawler";
-import { truncateStringByBytes } from "@/utils/truncateString";
+import { Crawler, Page } from "./crawler";
+import { truncateStringByBytes } from "@/utils/truncateString"
 
-const { chunkedUpsert, createIndexIfNotExists } = PineconeUtils;
+const { chunkedUpsert, createIndexIfNotExists } = PineconeUtils
 
 interface SeedOptions {
-  splittingMethod: string;
-  chunkSize: number;
-  chunkOverlap: number;
+  splittingMethod: string
+  chunkSize: number
+  chunkOverlap: number
 }
 
-type DocumentSplitter = RecursiveCharacterTextSplitter | MarkdownTextSplitter;
+type DocumentSplitter = RecursiveCharacterTextSplitter | MarkdownTextSplitter
 
-async function seed(csvUrl: string, limit: number, indexName: string, options: SeedOptions) {
+async function seed(url: string, limit: number, indexName: string, options: SeedOptions) {
   try {
     const pinecone = await getPineconeClient();
     const { splittingMethod, chunkSize, chunkOverlap } = options;
-    const crawler = new CsvCrawler(limit || 100);
-    const persons = await crawler.crawl(csvUrl) as Person[];
-    const splitter: DocumentSplitter = splittingMethod === 'recursive'
-      ? new RecursiveCharacterTextSplitter({ chunkSize, chunkOverlap })
-      : new MarkdownTextSplitter({});
-    const documents = await Promise.all(persons.map(person => prepareDocument(person, splitter)));
+    const crawler = new Crawler();
+    const page = await crawler.crawl(url);
+    const pages = [page]; // Wrap the single page in an array to work with the rest of the code
+    const splitter: DocumentSplitter = splittingMethod === 'recursive' ?
+      new RecursiveCharacterTextSplitter({ chunkSize, chunkOverlap }) : new MarkdownTextSplitter({});
+    const documents = await Promise.all(pages.map(page => prepareDocument(page, splitter)));
     await createIndexIfNotExists(pinecone!, indexName, 1536);
     const index = pinecone && pinecone.Index(indexName);
     const vectors = await Promise.all(documents.flat().map(embedDocument));
-    await chunkedUpsert(index!, vectors, csvUrl, 10);
+    await chunkedUpsert(index!, vectors, '', 10);
     return documents[0];
   } catch (error) {
     console.error("Error seeding:", error);
@@ -39,7 +39,6 @@ async function seed(csvUrl: string, limit: number, indexName: string, options: S
 
 async function embedDocument(doc: Document): Promise<Vector> {
   try {
-    const timestamp = generateTimestampAsString();
     const embedding = await getEmbeddings(doc.pageContent);
     const hash = md5(doc.pageContent);
     return {
@@ -48,47 +47,50 @@ async function embedDocument(doc: Document): Promise<Vector> {
       metadata: {
         chunk: doc.pageContent,
         text: doc.metadata.text as string,
-        hash: doc.metadata.hash as string,
-        Name: doc.metadata.Name as string,
-        Class: doc.metadata.Class as string,
-        URL: doc.metadata.URL as string,
-        Email: doc.metadata.Email as string,
-        Company: doc.metadata.Company as string,
-        Position: doc.metadata.Position as string,
-        Location: doc.metadata.Location as string,
-        Skills: doc.metadata.Skills as string,
-        PastJobTitles: doc.metadata.PastJobTitles as string,
-        Organizations: doc.metadata.Organizations as string,
-        About: doc.metadata.About as string,
-        timestamp: timestamp as string,
-      },
+        url: doc.metadata.url as string,
+        hash: doc.metadata.hash as string
+      }
     } as Vector;
   } catch (error) {
-    console.log("Error embedding document: ", error);
-    throw error;
+    console.log("Error embedding document: ", error)
+    throw error
   }
 }
 
-async function prepareDocument(person: Person, splitter: DocumentSplitter): Promise<Document[]> {
-  const pageContent = `${person.Name} ${person.Class} ${person.URL} ${person.Email} ${person.Company} ${person.Position} ${person.Location} ${person.Skills} ${person.PastJobTitles} ${person.Organizations} ${person.About}`;
-  const docs = await splitter.splitDocuments([
-    new Document({
-      pageContent,
-      metadata: {
-        text: truncateStringByBytes(pageContent, 36000),
-        ...person
-      },
-    }),
-  ]);
-  return docs.map((doc: Document) => {
-    return {
-      pageContent: doc.pageContent,
-      metadata: {
-        ...doc.metadata,
-        hash: md5(doc.pageContent),
-      },
-    };
-  });
+async function prepareDocument(page: Page, splitter: DocumentSplitter): Promise<Document[]> {
+  const documents: Document[] = [];
+
+  // Loop over each object in the page content array
+  for (const obj of page.content) {
+    // Convert the object to a string
+    const objStr = JSON.stringify(obj);
+
+    // Split the document
+    const docs = await splitter.splitDocuments([
+      new Document({
+        pageContent: objStr,
+        metadata: {
+          url: page.url,
+          text: truncateStringByBytes(objStr, 36000)
+        },
+      }),
+    ]);
+
+    // Add a hash to the document metadata and add it to the documents array
+    documents.push(
+      ...docs.map((doc: Document) => {
+        return {
+          pageContent: doc.pageContent,
+          metadata: {
+            ...doc.metadata,
+            hash: md5(doc.pageContent)
+          },
+        };
+      })
+    );
+  }
+
+  return documents;
 }
 
 export default seed;
